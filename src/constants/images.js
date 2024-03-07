599c2b44a905bc3b912f1f5e65a6cb5790701a95
faris-marhoon-nv22136 <149056472+faris-marhoon-nv22136@users.noreply.github.com>
2024-03-07 08:20:00 +0000

    7 - CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assests/Logo.svg";
 import HeroImage from "../assests/HeroImage.svg";
 import Post1Image from "/workspaces/blog-project/src/assests/posts/posts1.jpg";
 import PostProfileImage from "/workspaces/blog-project/src/assests/posts/posts-profile.svg";
+import CtaImage from "../assests/CtaImage.png";
 
 const images = {
   Logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
+  CtaImage,
 };
 
 export default images;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import MainLayout from '../../Components/MainLayout';
 import Hero from './container/Hero';
 import Articles from './container/Articles';
+import CTA from './container/CTA';
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
       <Articles />
+      <CTA />
     </MainLayout>
   );
 };
